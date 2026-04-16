a76bba29cb75ea4f22cff650e48549f6a29cef45
RyUk346 <mahmudur346@gmail.com>
2026-04-16 14:39:34 +0600

    deploy

--- src/app/friends/[id]/page.jsx
+++ src/app/friends/[id]/page.jsx
@@ -13,7 +13,9 @@ import {
 } from "react-icons/pi";
 import { FaRegTrashAlt } from "react-icons/fa";
 const friendPromise = async function () {
-  const res = await fetch("http://localhost:3000/friends.json");
+  const res = await fetch("http://localhost:3000/friends.json", {
+    cache: "no-store",
+  });
   const data = await res.json();
   return data;
 };
